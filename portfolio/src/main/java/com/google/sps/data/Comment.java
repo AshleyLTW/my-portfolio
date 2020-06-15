424d9fd1c35102df4447a06c3231b34df4752182
Ashley Wong <ashleywong@college.harvard.edu>
2020-06-15 05:29:48 +0000

    create blobstore upload servlet

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -5,12 +5,14 @@ public final class Comment {
     private final String text;
     private final String username;
     private final String mood;
+    private final String imageURL;
     private final long timestamp;
 
-    public Comment(String text, String username, String mood, long timestamp) {
+    public Comment(String text, String username, String mood, String imageURL, long timestamp) {
         this.text = text;
         this.username = username;
         this.mood = mood;
+        this.imageURL = imageURL;
         this.timestamp = timestamp;
     }
 }
